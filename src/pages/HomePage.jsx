0f5bc1f61a90d9bdffbb15b7fb29ef4042e5fdac
nottysukku <sukritchopra76@gmail.com>
2024-09-14 17:45:46 +0530

    change-stuff

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -55,28 +55,26 @@ const HomePage = ({ onTransitionToGameboard, onTransitionToGameboardAI }) => {
           </div>
         </div>
       ) : (
-        <div className='w-3/5 h-screen flex flex-col text-white font-extrabold lg:text-5xl text-sm bg-slate-900'>
-          <div className='text-center'>Snakes and Ladders</div>
-          <img src='https://i.postimg.cc/VNb6vCck/snakesandladders.png' className='h-44 mt-10 w-1/2 m-auto lg:h-96' alt='' />
+        <div className='lg:w-3/5 h-screen flex flex-col text-white font-extrabold lg:text-5xl text-sm bg-slate-900'>
+          <div className='text-center lg:block hidden'>Snakes and Ladders</div>
+          <img src='https://i.postimg.cc/VNb6vCck/snakesandladders.png' className='h-44 lg:mt-10 lg:w-1/2 m-auto lg:h-96' alt='' />
           <div className='flex flex-col h-full mt-5 '>
             <div
               onClick={handleGameAI} // Ensure this function is correctly connected
-              className='text-center justify-center bg-slate-700 lg:m-8 m-11 rounded-xl hover:bg-slate-800 cursor-pointer p-3 lg:p-0'
+              className='text-center justify-center bg-slate-700 lg:m-8 m-11 rounded-xl hover:bg-slate-800 cursor-pointer p-5 lg:p-0'
             >
               Single Player Vs AI
             </div>
             <div
               onClick={() => setShowForm(true)}
-              className='text-center justify-center bg-slate-700 lg:m-8 m-11 rounded-xl hover:bg-slate-800 cursor-pointer p-3 lg:p-0'
+              className='text-center justify-center bg-slate-700 lg:m-8 m-11 rounded-xl hover:bg-slate-800 cursor-pointer p-5 lg:p-0'
             >
               Player 1 Vs Player 2 (Couch-Mode)
             </div>
-            <div className='text-center justify-center bg-slate-700 lg:m-8 m-11 rounded-xl hover:bg-slate-800 cursor-pointer p-3 lg:p-0'>
-              Online Multiplayer
-            </div>
+            
             <div
               onClick={handleGo}
-              className='text-center justify-center bg-blue-700 rounded-xl hover:bg-blue-800 m-8 cursor-pointer p-3 lg:p-0'
+              className='text-center justify-center bg-blue-700 rounded-xl hover:bg-blue-800 m-8 cursor-pointer p-5 lg:p-0'
             >
               Back Home?
             </div>
